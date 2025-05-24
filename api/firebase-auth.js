07c2b4fab88cbb9a9af057d6f49134e196179fb8
Eloisa Antunes <eloisantunesb@gmail.com>
2025-05-24 16:59:17 -0300

    Config final db

--- api/firebase-auth.js
+++ api/firebase-auth.js
@@ -1,25 +1,19 @@
-const fetch = (...args) => 
-  import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = {
   getFirebaseAuthToken: async (email, password, apiKey) => {
-    try {
-      const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email, password, returnSecureToken: true })
-        }
-      );
+    if (!apiKey) throw new Error('Chave da API não configurada!');
+    
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password, returnSecureToken: true })
+    });
 
-      const data = await response.json();
-      if (data.error) throw new Error(data.error.message);
-      
-      return data.idToken;
-    } catch (error) {
-      console.error('Erro ao gerar token:', error);
-      throw error;
-    }
+    const data = await response.json();
+    if (data.error) throw new Error(data.error.message);
+    
+    return data.idToken;
   }
 };
