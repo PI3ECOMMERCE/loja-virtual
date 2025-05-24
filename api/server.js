17eb0ef389d5fd47c81ab2cb3f9877c7921f3703
Eloisa Antunes <eloisantunesb@gmail.com>
2025-05-24 09:19:06 -0300

    Atualiza api

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Configuração do Firebase (incluindo Auth)
-const serviceAccount = require('service-account-key.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 // Inicializa o Firebase Admin SDK
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const auth = admin.auth(); // Firebase Auth
