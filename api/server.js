40dbcc234aea0d756c7c93eb28f64e8f8565c392
Eloisa Antunes <eloisantunesb@gmail.com>
2025-05-24 13:54:53 -0300

    Ajuste api server.js

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Configuração do Firebase
-const serviceAccount = require('./service-account-key.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
