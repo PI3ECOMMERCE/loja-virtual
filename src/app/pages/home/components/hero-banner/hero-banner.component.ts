67430f42878b638f21008a7bd1d7d012858e64e7
PI3ECOMMERCE <eloisantunes@outlook.com>
2025-05-25 08:34:06 -0300

    Update hero-banner.component.ts

--- src/app/pages/home/components/hero-banner/hero-banner.component.ts
+++ src/app/pages/home/components/hero-banner/hero-banner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  selector: 'app-hero-banner', // Deve bater com a tag usada
+  selector: 'app-hero-banner',
   template: `
     <section class="hero" style="background:#9FC131; padding: 4rem; text-align: center; color: #fff;">
       <h1>{{ titulo }}</h1>
